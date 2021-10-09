af7548e0c77f5bfbb93c450814817e44f6868de2
MissGod1 <missgodbaby@gmail.com>
2021-10-09 19:26:47 +0800

    base pytorch+cu111

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo service ssh start
-PATH=$PATH:~/.local/bin jupyter lab --ip=0.0.0.0
--- run.sh
+++ run.sh
@@ -4,40 +4,22 @@ export COMPOSE_PROJECT_NAME=$(whoami)
 
 case $1 in
 build)
-    case $2 in
-    cpu|gpu)
-        # uid=$(id -u)
-        # name=$(whoami)
-        chmod 777 entrypoint.sh
-        read -p 'Please set root & jupyter password:' password
-        docker-compose build --build-arg VERSION=$2 \
-        --build-arg NAME=$(whoami) \
-        --build-arg PASSWORD=$password \
-        --build-arg UID=$(id -u) \
-        $2
-        ;;
-    *)
-        echo "$0 build cpu|gpu"
-        ;;
-    esac
+    read -p 'Please set user password:' password
+    docker-compose build \
+    --build-arg NAME=$(whoami) \
+    --build-arg PASSWORD=$password \
+    --build-arg UID=$(id -u)
     ;;
 start)
-    case $2 in
-    cpu|gpu)
-        if [ ! -d "code/" ];then
-            mkdir code
-        fi
-        docker-compose up $2
-        ;;
-    *)
-        echo "$0 start cpu|gpu"
-        ;;
-    esac
+    if [ ! -d "code/" ];then
+        mkdir code
+    fi
+    docker-compose up
     ;;
 remove)
-    docker-compose down $2
+    docker-compose down
     ;;
 *)
-    echo "$0 [build|start|remove] [cpu|gpu]"
+    echo "$0 [build|start|remove]"
     ;;
 esac
